01d3bb488f81c78b80c20aeed9ce61890510090c
Oilujpy <82006857+Oilujpy@users.noreply.github.com>
2025-05-10 16:01:44 -0400

    feat: the display now shows the button clicked
    - The C button, which stands for clear, removes all the content from the
    result display.
    - The operations buttons are not mapped to their fomula yet. So far,
    they are only displayed when clicked.

--- script.js
+++ script.js
@@ -34,6 +34,87 @@ function divide(n1, n2) {
     }
 }
 
+function updateCanvas(targetText) {
+    let resultDiv = document.querySelector(".cal-result");
+    if (targetText === 'C') {
+        resultDiv.innerHTML = "";
+    } else {
+        resultDiv.textContent += targetText;
+    }
+}
+
+let buttons = document.querySelector(".cal-buttons");
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case 'num0':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num1':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num2':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num3':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num4':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num5':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num6':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num7':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num8':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'num9':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'divide':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'multiply':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'substract':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'add':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'result':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+        case 'clear':
+            console.log(`You pressed ${target.textContent}`);
+            updateCanvas(target.textContent);
+            break;
+    }
+})
+
 function operate(num1, operation, num2) {
     switch (operation.toLowerCase()) {
         case "+":
